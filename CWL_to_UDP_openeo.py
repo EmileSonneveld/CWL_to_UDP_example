e8239888c7c41787ed8aebd54c9ac78886482433
Emile Sonneveld <emile.sonneveld@vito.be>
2026-04-07 15:36:50 +0200

    Better example code. Update README.

--- CWL_to_UDP_openeo.py
+++ CWL_to_UDP_openeo.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 import openeo
@@ -5,23 +6,24 @@ import openeo
 url = "https://openeo.dataspace.copernicus.eu"
 connection = openeo.connect(url).authenticate_oidc()
 
-# datacube = connection.load_collection(
-#     "SENTINEL2_L2A",
-#     spatial_extent={"east": 5.08, "north": 51.22, "south": 51.215, "west": 5.07},
-#     temporal_extent=["2023-06-01", "2023-06-06"],
-#     bands=["B04", "B03", "B02"],
-# )
-# output_path = Path("tmp_load_collection_output")
-
-# This simple CWL wilt output unscaled sentinel2 STAC data, quite like the load_collection equivalent:
-datacube = connection.datacube_from_process(
-    "run_udf",
-    data=None,
-    udf="https://raw.githubusercontent.com/Open-EO/openeo-geotrellis-kubernetes/master/openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/cwl/dummy_stac.cwl",
-    runtime="EOAP-CWL",
-    context={},
-)
-output_path = Path("tmp_run_udf_output")
+if False:  # Toggle here what example to run!
+    datacube = connection.load_collection(
+        "SENTINEL2_L2A",
+        spatial_extent={"east": 5.08, "north": 51.22, "south": 51.215, "west": 5.07},
+        temporal_extent=["2023-06-01", "2023-06-06"],
+        bands=["B04", "B03", "B02"],
+    )
+    output_path = Path("tmp_load_collection_output")
+else:
+    # This simple CWL wilt output unscaled sentinel2 STAC data, quite like the load_collection equivalent:
+    datacube = connection.datacube_from_process(
+        "run_udf",
+        data=None,
+        udf="https://raw.githubusercontent.com/Open-EO/openeo-geotrellis-kubernetes/master/openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/cwl/dummy_stac.cwl",
+        runtime="EOAP-CWL",
+        context={},
+    )
+    output_path = Path("tmp_run_udf_output")
 
 datacube = connection.datacube_from_process(
     "linear_scale_UDP",
@@ -30,6 +32,6 @@ datacube = connection.datacube_from_process(
 )
 
 output_path.mkdir(exist_ok=True)
-job = datacube.create_job(title=__file__)
+job = datacube.create_job(title=os.path.basename(__file__) + " " + output_path.name)
 job.start_and_wait()
 job.get_results().download_files(output_path)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CWL_to_UDP_example
+
+run_udf with a CWL can be seen as a kind of load_stac / load_collection, but with a CWL workflow to acces/generate the
+data.
+
+These proces graphs are both valid:
+![image](./docs/load_collection_linear_scale.png)
+and:
+![image](./docs/run_udf_linear_scale.png)
